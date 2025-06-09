a8f279d428aadd8c98cb09db055b86eda131a0c2
jhp922 <stan090981@gmail.com>
2025-06-09 21:47:51 +0900

    Update character.js

--- character.js
+++ character.js
@@ -1,26 +1,4 @@
-// character.js - 캐릭터 등장, 걷기, 전환 관련 로직
-
-let standImgs = [], walkImgs = [];
-let currentAge = 0;
-let characterX, characterY;
-let isGiven = false;
-let frameToggle = false;
-
-let characterAppearAnim = false;
-let characterAppearFrame = 0;
-let characterAppearDuration = 32;
-let characterAppearDone = false;
-
-function drawCharacter(x, y, isGiven, ageIndex) {
-  let w = 100 * scaleX;
-  let h = 100 * scaleY;
-  if (!isGiven) {
-    image(standImgs[ageIndex], x, y, w, h);
-  } else {
-    if (frameCount % 6 === 0) frameToggle = !frameToggle;
-    image(frameToggle ? walkImgs[ageIndex] : standImgs[ageIndex], x, y, w, h);
-  }
-}
+// --- 캐릭터 등장 애니메이션 및 출력 (character.js) ---
 
 function drawCharacterAppearAnim() {
   let img = standImgs[currentAge];
@@ -56,6 +34,17 @@ function drawCharacterAppearAnim() {
   }
 }
 
+function drawCharacter(x, y, isGiven, ageIndex) {
+  let w = 100 * scaleX;
+  let h = 100 * scaleY;
+  if (!isGiven) {
+    image(standImgs[ageIndex], x, y, w, h);
+  } else {
+    if (frameCount % 6 === 0) frameToggle = !frameToggle;
+    image(frameToggle ? walkImgs[ageIndex] : standImgs[ageIndex], x, y, w, h);
+  }
+}
+
 function nextCharacter() {
   sence++;
   switch (sence) {
@@ -65,15 +54,13 @@ function nextCharacter() {
     case 4: currentAge = 2; break;
     case 5: currentAge = 3; break;
   }
-
   characterX = 40 * scaleX;
   isGiven = false;
   objectVisible = true;
-  objectX = width - 200 * scaleX;
+  objectX = width - 200 * scaleX; // 화면 오른쪽 밖에서 시작
   objectY = height - 100 * scaleY;
   characterAppearAnim = true;
   characterAppearFrame = 0;
   characterAppearDone = false;
-
-  interactiveTrees = []; // 나무 클릭 초기화 (sence 2, 3용)
 }
+
